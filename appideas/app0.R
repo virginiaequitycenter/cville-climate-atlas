a321b62b97df4be485c9785798d84c213298cdf1
jacob-gg <jrg2xq@virginia.edu>
2022-02-14 11:47:34 -0500

    use static legend in leaflet map

--- appideas/app0.R
+++ appideas/app0.R
@@ -26,18 +26,30 @@ df <- readRDS("data/cvl_data.RDS")
 varlist <- df %>% select(where(is.numeric)) %>% names()
 
 # read geometry
-geo <- readRDS('data/cvl_data_geo.RDS') %>% select(locality, geometry)
+geo <- readRDS('data/cvl_data_geo.RDS') %>% select(geoid, locality, geometry)
 
 # create palette for use in bichoropleth palette function
 bipal <- c("#e8e8e8", "#dfd0d6", "#be64ac", # A-1, A-2, A-3,
            "#ace4e4", "#a5add3", "#8c62aa", # B-1, B-2, B-3
            "#5ac8c8", "#5698b9", "#3b4994") # C-1, C-2, C-3
 
-# create palette object for logo generation
+# create palette object for legend generation
 bipal_logo <- tibble("3-3" = "#3b4994", "2-3" = "#8c62aa", "1-3" = "#be64ac",
                      "3-2" = "#5698b9", "2-2" = "#a5add3", "1-2" = "#dfd0d6",
                      "3-1" = "#5ac8c8", "2-1" = "#ace4e4", "1-1" = "#e8e8e8") %>%
   gather("group", "fill")
+# generate and save legend svg
+bipal_ <- bipal_logo %>%
+  separate(group, into = c('x', 'y'), sep = "-") %>%
+  mutate(x = as.integer(x),
+         y = as.integer(y))
+legend_ <- ggplot() +
+  geom_tile(data = bipal_, mapping = aes(x = x, y = y, fill = fill)) +
+  scale_fill_identity() + theme_void() + coord_fixed() +
+  labs(x = paste0("Higher X variable \u2192"), # Check if these are right in terms of y/x axes
+       y = paste0("Higher Y variable \u2192")) +
+  theme(axis.title = element_text(size = 6), axis.title.y = element_text(angle = 90))
+ggsave(plot = legend_, filename = 'www/bivariate_legend.svg', width = 1, height = 1)
 
 # ....................................
 # Define User Interface ----
@@ -80,7 +92,8 @@ server <- function(input, output) {
       dplyr::select(x = !!sym(input$indicator1),
                     y = !!sym(input$indicator2),
                     locality = locality,
-                    pop = totalpopE)
+                    pop = totalpopE,
+                    geoid = geoid)
   })
 
   # produce scatterplot
@@ -91,25 +104,11 @@ server <- function(input, output) {
 
   # rebuild map whenever input variables are updated
   output$leaf <- renderLeaflet({
-    # generate and save new legend svg
-    bipal_ <- bipal_logo %>%
-      separate(group, into = c(input$indicator1, input$indicator2), sep = "-") %>%
-      mutate(x = as.integer(eval(sym(input$indicator1))),
-             y = as.integer(eval(sym(input$indicator2))))
-    legend_ <- ggplot() +
-      geom_tile(data = bipal_, mapping = aes(x = x, y = y, fill = fill)) +
-      scale_fill_identity() + theme_void() + coord_fixed() +
-      labs(x = paste0("Higher ", input$indicator1, " \u2192"), # Check if these are right in terms of y/x axes
-           y = paste0("Higher ", input$indicator2, " \u2192")) +
-      theme(axis.title = element_text(size = 6), axis.title.y = element_text(angle = 90))
-    ggsave(plot = legend_, filename = 'www/bivariate_legend.svg', width = 1, height = 1)
-
     # merge reactive data with geometry, get bi_class values, and generate palette for Leaflet
-    to_map <- merge(data(), geo, by = 'locality')
+    to_map <- merge(data(), geo, by = 'geoid')
     to_map <- bi_class(to_map, x = x, y = y, style = "quantile", dim = 3)
     to_map <- st_transform(st_as_sf(to_map), 4326)
     factpal <- colorFactor(bipal, domain = to_map$bi_class)
-
     leaflet() %>%
       addProviderTiles("CartoDB.Positron") %>%
       addPolygons(data = to_map,
